afbce12606c4470d981214af67782de2ef41cc52
Nirav <nirav.j05@gmail.com>
2016-09-18 12:21:32 +0530

    Changes in DB line where table error reported
    Table error reported during restore fixed it for sds and ods to tags and compare.

--- JbossDB.ps1
+++ JbossDB.ps1
@@ -4,330 +4,310 @@ $a = new-object -comobject wscript.shell
 
 write $jboss
 if ($jboss) 
- {
-
-	$intAnswer = $a.popup("Do you want to Stop Java Jboss Process It is recomended to Stop Jboss before you start import of DB?",0,"Jboss is running !!!!",4) 
-	If ($intAnswer -eq 6) 
-	{ 
-		$a.popup("Stopping Joboss") 
-		$jboss | Stop-Process -Force
-
-	}
-
-
-#Drop Al Databases
-"
-#####################################################
-Droping all App related DB hope you have made backup
-#####################################################
-"
-&cmd /c "mysql --user=root --password=gravitant mysql  < DropAllDB.SQL"
-#Crete all DB Schema
-"
-#####################################################
-Creating all new schema for app related DB
-#####################################################
-
-"
-&cmd /c "mysql --user=root --password=gravitant mysql  < CreateSchm.SQL"
-
-
-
-
-$colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly 
-
-foreach ($objFile in $colFiles)
-{
-	
-	
-	#bts DB import
-    if ($objFile.Name -match "bts")
     {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant bts   <" $objFile.FullName
-    }
-}
 
-	
-foreach ($objFile in $colFiles)
-{	#sds db import 
-	if ($objFile.Name -match "sds")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant sds   <" $objFile.FullName
-    }
-}		
+	        $intAnswer = $a.popup("Do you want to Stop Java Jboss Process It is recomended to Stop Jboss before you start import of DB?",0,"Jboss is running !!!!",4) 
+	        If ($intAnswer -eq 6) 
+	        { 
+		        $a.popup("Stopping Joboss") 
+		        $jboss | Stop-Process -Force
+
+	        }
 
 
-foreach ($objFile in $colFiles)
-{
-	#ODS db import 
-	if ($objFile.Name -match "ods")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant ods   <" $objFile.FullName
-    }
-}
+            #Drop Al Databases
+            "
+            #####################################################
+            Droping all App related DB hope you have made backup
+            #####################################################
+            "
+            &cmd /c "mysql --user=root --password=gravitant mysql  < DropAllDB.SQL"
+            #Crete all DB Schema
+            "
+            #####################################################
+            Creating all new schema for app related DB
+            #####################################################
 
-foreach ($objFile in $colFiles)
-{		
-	#sfb db import 
-	if ($objFile.Name -match "sfb")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant sfb   <" $objFile.FullName
-    }
-}	
-
-foreach ($objFile in $colFiles)
-{
-	#tags db import 
-	if ($objFile.Name -match "tags")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant tags   <" $objFile.FullName
-    }
-}
+            "
+            &cmd /c "mysql --user=root --password=gravitant mysql  < CreateSchm.SQL"
 
 
-foreach ($objFile in $colFiles)
-{	
-    #softlayer DB import
-    if ($objFile.Name -match "softlayer")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant softlayer   <" $objFile.FullName
-    }
-}
-
-foreach ($objFile in $colFiles)
-
-{		
-	#demo db import
-	if ($objFile.Name -match "demo")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant demo   <" $objFile.FullName
-    }
+            #Database Import will start
+                
+                #bts database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *bts*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant bts   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any bts file"
+                    }
 
 
-}
-
-foreach ($objFile in $colFiles)
-
-{		
-	#demo db import
-	if ($objFile.Name -match "pyscreener")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant pyscreener   <" $objFile.FullName
-    }
-
-
-}
-
-foreach ($objFile in $colFiles)
-
-{		
-	#demo db import
-	if ($objFile.Name -match "comparedb")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant comparedb   <" $objFile.FullName
-    }
+                #tags database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *tags*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant tags   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any tags file"
+                    }
+                
+                #cme database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *cme*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant cme   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any cme file"
+                    }
 
 
-}
+                #sds database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *sds*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant sds   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any sds file"
+                    }
 
+                #ods database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *ods*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant ods   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any ods file"
+                    }
 
-
-#Update bts tables
-"
-#####################################################
-Updating bts database tables to values for local instance
-#####################################################
-"
-
-&cmd /c "mysql --user=root --password=gravitant bts   <" UpdateBts.sql
-    
-
-"
-##########################################################################################################
-All tasks are done now just go ahead and restart jboss server from eclipse to start CM instance.
-##########################################################################################################
-"
+                #sfb database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *sfb*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant sfb   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any sfb file"
+                    }
 	
 
-    
-   
+                #softlayer database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *softlayer*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant softlayer   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any softlayer file"
+                    }
 	
-}
+                #demo database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *demo*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant demo   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any demo file"
+                    }
+                #pyscreener  database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *pyscreener*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant pyscreener   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any pyscreener file"
+                    }	
+	
+	
+                #compare database
+                if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *compare*)
+                    {
+                    Write-Host $colFiles
+	                cmd /c  "mysql --user=root --password=gravitant comparedb   <" $colFiles.FullName
+                    }
+                else
+                    {
+                    Write-Host "I did not get any comparedb file"
+                    }
+
+
+	
+    }
 
 
 else
-{
-$a.popup("No Jboss is running") 
+    {
+
+
+            $a.popup("No Jboss is running") 
 	
-Remove-Variable jboss
+            Remove-Variable jboss
 
-#Drop Al Databases
-"
-#####################################################
-Droping all App related DB hope you have made backup
-#####################################################
-"
-&cmd /c "mysql --user=root --password=gravitant mysql  < DropAllDB.SQL"
-#Crete all DB Schema
-"
-#####################################################
-Creating all new schema for app related DB
-#####################################################
+            #Drop Al Databases
+            "
+            #####################################################
+            Droping all App related DB hope you have made backup
+            #####################################################
+            "
+            &cmd /c "mysql --user=root --password=gravitant mysql  < DropAllDB.SQL"
 
-"
-&cmd /c "mysql --user=root --password=gravitant mysql  < CreateSchm.SQL"
+            #Crete all DB Schema
+            "
+            #####################################################
+            Creating all new schema for app related DB
+            #####################################################
+
+            "
+            &cmd /c "mysql --user=root --password=gravitant mysql  < CreateSchm.SQL"
+
+                    #bts database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *bts*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant bts   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any bts file"
+                        }
 
 
+                    #tags database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *tags*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant tags   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any tags file"
+                        }
+                    #cme database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *cme*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant cme   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any cme file"
+                        }
 
+                    #sds database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *sds*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant sds   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any sds file"
+                        }
 
-$colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly 
+                    #ods database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *ods*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant ods   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any ods file"
+                        }
 
-foreach ($objFile in $colFiles)
-{
+                    #sfb database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *sfb*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant sfb   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any sfb file"
+                        }
+	
+
+                    #softlayer database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *softlayer*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant softlayer   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any softlayer file"
+                        }
+	
+                    #demo database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *demo*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant demo   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any demo file"
+                        }
+                    #pyscreener  database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *pyscreener*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant pyscreener   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any pyscreener file"
+                        }	
 	
 	
-	#bts DB import
-    if ($objFile.Name -match "bts")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant bts   <" $objFile.FullName
-    }
-}
-
-	
-foreach ($objFile in $colFiles)
-{	#sds db import 
-	if ($objFile.Name -match "sds")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant sds   <" $objFile.FullName
-    }
-}		
+                    #comparedb database
+                    if ($colFiles = Get-ChildItem C:\DBAutomation\DBDropFilesOnly -Filter *comparedb*)
+                        {
+                        Write-Host $colFiles
+	                    cmd /c  "mysql --user=root --password=gravitant comparedb   <" $colFiles.FullName
+                        }
+                    else
+                        {
+                        Write-Host "I did not get any comparedb file"
+                        }
+          
 
 
-foreach ($objFile in $colFiles)
-{
-	#ODS db import 
-	if ($objFile.Name -match "ods")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant ods   <" $objFile.FullName
-    }
-}
+            #Update bts tables
+            "
+            #####################################################
+            Updating bts database tables to values for local instance
+            #####################################################
+            "
 
-foreach ($objFile in $colFiles)
-{		
-	#sfb db import 
-	if ($objFile.Name -match "sfb")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant sfb   <" $objFile.FullName
-    }
-}	
-
-foreach ($objFile in $colFiles)
-{
-	#tags db import 
-	if ($objFile.Name -match "tags")
-    {
-    Write-Host $objFile
-	cmd /c  "mysql --user=root --password=gravitant tags   <" $objFile.FullName
-    }
-}
-
-
-foreach ($objFile in $colFiles)
-{	
-    #softlayer DB import
-    if ($objFile.Name -match "softlayer")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant softlayer   <" $objFile.FullName
-    }
-}
-
-foreach ($objFile in $colFiles)
-
-{		
-	#demo db import
-	if ($objFile.Name -match "demo")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant demo   <" $objFile.FullName
-    }
-
-
-}
-
-foreach ($objFile in $colFiles)
-
-{		
-	#demo db import
-	if ($objFile.Name -match "pyscreener")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant pyscreener   <" $objFile.FullName
-    }
-
-
-}
-
-foreach ($objFile in $colFiles)
-
-{		
-	#demo db import
-	if ($objFile.Name -match "comparedb")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant comparedb   <" $objFile.FullName
-    }
-
-
-}
-
-
-
-foreach ($objFile in $colFiles)
-
-{		
-	#demo db import
-	if ($objFile.Name -match "cme")
-    {
-    Write-Host $objFile
-	cmd /c "mysql --user=root --password=gravitant cme   <" $objFile.FullName
-    }
-
-
-}
-
-
-#Update bts tables
-"
-#####################################################
-Updating bts database tables to values for local instance
-#####################################################
-"
-
-&cmd /c "mysql --user=root --password=gravitant bts   <" UpdateBts.sql
+            &cmd /c "mysql --user=root --password=gravitant bts   <" UpdateBts.sql
     
 
-"
-##########################################################################################################
-All tasks are done now just go ahead and restart jboss server from eclipse to start CM instance.
-##########################################################################################################
-"
+            "
+            ##########################################################################################################
+            All tasks are done now just go ahead and restart jboss server from eclipse to start CM instance.
+            ##########################################################################################################
+            "
 	
 
     
    
 
-}
+    }
